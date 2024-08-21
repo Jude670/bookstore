44e24d6b3082687eb0b88f850568a21425dd8c72
Jude670 <lawrencelarry660@gmail.com>
2024-08-21 05:13:00 +0100

    updated

--- auth/register.php
+++ auth/register.php
@@ -2,7 +2,14 @@
 <?php require "../config/config.php"; ?>
 <?php
 
-     if(isset($_POST['submit'])) {
+
+    if(isset($_SESSION['username'])) {
+        header("location: ".APPURL."");
+    }
+
+
+
+    if(isset($_POST['submit'])) {
         // echo "<script>success</script>";
         if(empty($_POST['username']) OR empty($_POST['email']) OR empty($_POST['password'])) {
             echo "<script>alert('one or more inputs are empty'); </script>";
